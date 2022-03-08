f23f520666c73a780cd915670c60c5df47af22b9
Ae-Mc <ae_mc@mail.ru>
2022-03-09 00:58:45 +0300

    feat(router): remove counter feature and update router to use new pages

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,13 +1,20 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:road_to_the_dream/app/router/guards/init_guard.dart';
-import 'package:road_to_the_dream/features/counter/presentation/pages/counter_page.dart';
+import 'package:road_to_the_dream/core/root_page.dart';
 import 'package:road_to_the_dream/features/splash/presentation/pages/splash_page.dart';
+import 'package:road_to_the_dream/features/tasks/presentation/pages/categories_page.dart';
 
 @AdaptiveAutoRouter(
   replaceInRouteName: 'Page,Route',
   routes: [
-    AutoRoute(page: SplashPage),
-    AutoRoute(page: CounterPage, initial: true, guards: [InitGuard]),
+    AutoRoute(
+      page: RootPage,
+      initial: true,
+      children: [
+        AutoRoute(page: CategoriesPage, initial: true, guards: [InitGuard]),
+        AutoRoute(page: SplashPage),
+      ],
+    ),
   ],
 )
 class $AppRouter {}
--- lib/features/counter/presentation/cubit/counter_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:injectable/injectable.dart';
-
-@injectable
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
-
-  void increment() => emit(state + 1);
-}
