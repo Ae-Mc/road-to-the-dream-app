f9835d492ea2d71f87ce3a7d4859310fcd66b6a3
Ae-Mc <ae_mc@mail.ru>
2022-03-10 14:17:58 +0300

    refactor(tasks): move range string computation to separate extension to Task entity

--- lib/features/tasks/domain/entities/task.dart
+++ lib/features/tasks/domain/entities/task.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intl/intl.dart';
 import 'package:road_to_the_dream/features/tasks/domain/entities/uuid.dart';
 
 part 'task.freezed.dart';
@@ -11,4 +12,38 @@ class Task with _$Task {
     DateTime? start,
     DateTime? end,
   }) = _Task;
+}
+
+extension TaskDateRange on Task {
+  String getDateRangeString() {
+    final start = this.start;
+    final end = this.end;
+    if (start == null) {
+      return '';
+    }
+    if (end == null) {
+      return (start.isMidnight
+          ? DateFormat('d MMMM').format(start)
+          : DateFormat('d MMMM. H:mm a').format(start));
+    }
+
+    if (start.year == end.year) {
+      if (start.month == end.month) {
+        return '${start.day}-${DateFormat("d MMMM").format(end)}';
+      } else {
+        final format = DateFormat('d MMMM');
+
+        return format.format(start) + ' - ' + format.format(end);
+      }
+    } else {
+      final format = DateFormat('d MMMM y');
+
+      return format.format(start) + ' - ' + format.format(end);
+    }
+  }
+}
+
+extension on DateTime {
+  bool get isMidnight =>
+      difference(DateTime(year, month, day)) == Duration.zero;
 }
--- lib/features/tasks/presentation/widgets/task_preview_row.dart
+++ lib/features/tasks/presentation/widgets/task_preview_row.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:road_to_the_dream/app/theme/bloc/app_theme.dart';
 import 'package:road_to_the_dream/features/tasks/domain/entities/task.dart';
 
@@ -15,9 +14,6 @@ class TaskPreviewRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final start = task.start;
-    final end = task.end;
-
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
@@ -36,13 +32,9 @@ class TaskPreviewRow extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(task.name, style: AppTheme.of(context).textTheme.title2),
-              if (start != null)
+              if (task.start != null)
                 Text(
-                  end == null
-                      ? (start.isMidnight
-                          ? DateFormat('d MMMM').format(start)
-                          : DateFormat('d MMMM. H:mm a').format(start))
-                      : buildDateDiffString(start, end),
+                  task.getDateRangeString(),
                   style: AppTheme.of(context)
                       .textTheme
                       .body1Regular
@@ -54,25 +46,4 @@ class TaskPreviewRow extends StatelessWidget {
       ],
     );
   }
-
-  String buildDateDiffString(DateTime start, DateTime end) {
-    if (start.year == end.year) {
-      if (start.month == end.month) {
-        return '${start.day}-${DateFormat("d MMMM").format(end)}';
-      } else {
-        final format = DateFormat('d MMMM');
-
-        return format.format(start) + ' - ' + format.format(end);
-      }
-    } else {
-      final format = DateFormat('d MMMM y');
-
-      return format.format(start) + ' - ' + format.format(end);
-    }
-  }
-}
-
-extension on DateTime {
-  bool get isMidnight =>
-      difference(DateTime(year, month, day)) == Duration.zero;
 }
