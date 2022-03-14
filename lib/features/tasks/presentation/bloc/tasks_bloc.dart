22f19b00dc2e267cc2d944d1a581386bd3ac4da3
Ae-Mc <ae_mc@mail.ru>
2022-03-14 15:54:38 +0300

    feat(tasks/bloc): write event handler function and implement most of Tasks logic

--- lib/features/tasks/presentation/bloc/tasks_bloc.dart
+++ lib/features/tasks/presentation/bloc/tasks_bloc.dart
@@ -1,53 +1,48 @@
 import 'package:bloc/bloc.dart';
-import 'package:road_to_the_dream/features/tasks/domain/entities/category.dart';
+import 'package:dartz/dartz.dart' hide Task;
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:logger/logger.dart';
+import 'package:road_to_the_dream/core/failure.dart';
 import 'package:road_to_the_dream/features/tasks/domain/entities/task.dart';
 import 'package:road_to_the_dream/features/tasks/domain/entities/uuid.dart';
-import 'package:injectable/injectable.dart';
+import 'package:road_to_the_dream/features/tasks/domain/repositories/tasks_repository.dart';
 import 'package:road_to_the_dream/features/tasks/presentation/bloc/tasks_event.dart';
 import 'package:road_to_the_dream/features/tasks/presentation/bloc/tasks_state.dart';
 
 @injectable
 class TasksBloc extends Bloc<TasksEvent, TasksState> {
-  TasksBloc()
-      : super(
-          TasksState.loaded(
-            categories: [
-              Category(
-                id: UUID(),
-                name: "Category 1",
-                color: 0xF1CACA,
-                tasks: [
-                  Task(
-                    id: UUID(),
-                    name: 'To do something',
-                    start: DateTime(2022, 3, 8),
-                    end: DateTime(2022, 3, 28),
-                  ),
-                ],
-              ),
-              Category(
-                id: UUID(),
-                name: "Category 2",
-                color: 0xBFE8E3,
-                tasks: [
-                  Task(
-                    id: UUID(),
-                    name: 'To do something else',
-                    start: DateTime(2022, 3, 8, 9, 0),
-                  ),
-                ],
-              ),
-              Category(
-                id: UUID(),
-                name: "Category 3",
-                color: 0xFFDFA2,
-                tasks: [
-                  Task(id: UUID(), name: 'Another to do something else'),
-                ],
-              ),
-            ],
-          ),
-        ) {
-    on<TasksEvent>((event, emit) => {});
+  final TasksRepository repository;
+
+  TasksBloc(this.repository)
+      : super(TasksState.loaded(categories: repository.load())) {
+    on<TasksEvent>(handleEvent);
+  }
+
+  Future<void> handleEvent(TasksEvent event, Emitter<TasksState> emit) async {
+    await event.when<Future<void>>(
+      categoryAdded: (category) async =>
+          updateState(await repository.addCategory(category), emit),
+      taskAdded: (UUID categoryID, Task task) async => updateState(
+        await repository.addTaskToCategory(categoryID, task),
+        emit,
+      ),
+      taskRemoved: (UUID taskID) async =>
+          updateState(await repository.removeTask(taskID), emit),
+      categoryRemoved: (UUID categoryID) async =>
+          updateState(await repository.removeCategory(categoryID), emit),
+    );
+  }
+
+  void updateState(Either<Failure, void> result, Emitter<TasksState> emit) {
+    return result.fold<void>(
+      (l) {
+        GetIt.I<Logger>().e('Error: $l');
+        emit(const TasksState.loaded(categories: []));
+      },
+      (r) {
+        emit(TasksState.loaded(categories: List.from(repository.load())));
+      },
+    );
   }
 }
